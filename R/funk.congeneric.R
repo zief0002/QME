f271f550fafe2ccb53f904804c70bd4d2693536f
canoemoore <moor0554@umn.edu>
2013-06-25 23:26:13 -0400

    Create funk.congeneric.R
    A function to estimate and compare congeneric reliability and internal consistency; requires sem and stringr libraries.

---
+++ R/funk.congeneric.R
@@ -0,0 +1,17 @@
+library(sem)
+library(stringr)
+funk.congeneric <- function(data.congeneric) {
+  data.congeneric <- as.data.frame(data.congeneric)
+  names(data.congeneric) <- make.names(names(data.congeneric))
+  name.tempfile <- tempfile()
+  model.cfa <- paste("F:", paste(names(data.congeneric), collapse = ", "))
+  cat(model.cfa, file = name.tempfile)
+  model.cfa <- cfa(file = name.tempfile, reference.indicators = FALSE)
+  unlink(name.tempfile)
+  cfa.out <- sem(model = model.cfa, data = data.congeneric)
+  names.loadings <- str_detect(names(cfa.out$coeff), fixed("lam["))
+  names.errors <- str_detect(names(cfa.out$coeff), fixed("V["))
+  r.congeneric <- sum(cfa.out$coeff[names.loadings]) ^ 2 / 
+    (sum(cfa.out$coeff[names.loadings]) ^ 2 + sum(cfa.out$coeff[names.errors]))
+  round(c("Congeneric" = r.congeneric, "Alpha" = alpha(cfa.out$S)$total$raw_alpha), 2)
+}
