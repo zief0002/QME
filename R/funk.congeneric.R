ebc0f6bc34b29704dd8550c75f3d664f9bc26378
Christopher Moore <moor0554@umn.edu>
2013-06-28 19:28:55 -0500

    Improvements to funk.congeneric and documentation.

--- R/funk.congeneric.R
+++ R/funk.congeneric.R
@@ -1,15 +1,19 @@
-funk.congeneric <- function(data.congeneric) {
-  data.congeneric <- as.data.frame(data.congeneric)
-  names(data.congeneric) <- make.names(names(data.congeneric))
+funk.congeneric <- function(x, N = NULL, vcov = TRUE) {
+  if(vcov == FALSE) {
+    colnames(x) <- make.names(colnames(x))
+    x <- var(x)
+  }
   name.tempfile <- tempfile()
-  model.cfa <- paste("F:", paste(names(data.congeneric), collapse = ", "))
+  model.cfa <- paste("F:", paste(colnames(x), collapse = ", "))
   cat(model.cfa, file = name.tempfile)
   model.cfa <- cfa(file = name.tempfile, reference.indicators = FALSE)
   unlink(name.tempfile)
-  cfa.out <- sem(model = model.cfa, data = data.congeneric)
+  cfa.out <- sem(model = model.cfa, S = x, N = N)
   names.loadings <- str_detect(names(cfa.out$coeff), fixed("lam["))
   names.errors <- str_detect(names(cfa.out$coeff), fixed("V["))
   r.congeneric <- sum(cfa.out$coeff[names.loadings]) ^ 2 / 
     (sum(cfa.out$coeff[names.loadings]) ^ 2 + sum(cfa.out$coeff[names.errors]))
-  c("Congeneric" = r.congeneric)
+  r.congeneric <- c("Congeneric" = r.congeneric)
+  attr(r.congeneric, "cfa") <- cfa.out
+  r.congeneric
 }
