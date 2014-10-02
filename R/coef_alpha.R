ed01a06ee70fd6e40009c1f4e2f5f35ece037817
Claudio Bustos <clbustos@gmail.com>
2014-10-02 01:20:47 -0300

    Speedup for Cronbach's alpha

--- R/coef_alpha.R
+++ R/coef_alpha.R
@@ -1,9 +1,5 @@
-coef_alpha <- function(x, k=NA , ...){
-  
-  k<-ifelse(is.na(k),length(x), k)  
-  var.x <- var(apply(x,1,sum))
-  sum.var.i <- sum(apply(x,2,var))
-  
-  coef.alpha <- (k/(k-1))* ((var.x - sum.var.i)/(var.x))
-  return(coef.alpha)
+coef_alpha <- function(x){
+  cov.matrix<-cov(x,use="p")
+  k<-nrow(cov.matrix)
+  return(k/(k-1) *(1- (sum(diag(cov.matrix)) / sum(cov.matrix) )))
 }
