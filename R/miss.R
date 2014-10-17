8a136e6f08e54bc8d9e9daf8631039e76aa28235
vuexx199 <vuexx199@umn.edu>
2014-10-17 16:58:43 -0500

    Update miss.R
    changed return for print

--- R/miss.R
+++ R/miss.R
@@ -3,14 +3,11 @@
 #################################################################################
 
 
-miss = function(x, order = FALSE, id = TRUE, ...){
+miss = function(x, order = FALSE,...){
   
-	column_start = ifelse(id == TRUE, 2, 1)
-
-
   # x is a data frame with and ID variable column of raw responses
   # assume that missing can be detected; so no blank spaces as levels
-  missingness = apply(x[column_start:length(x)], MARGIN = 2, function(x) sum(is.na(x)))
+  missingness = apply(x[ , 1:length(x)], MARGIN = 2, function(x) sum(is.na(x)))
   
   # store data into a single data frame 
 	missData = data.frame(
@@ -18,25 +15,21 @@ miss = function(x, order = FALSE, id = TRUE, ...){
 		numMiss = missingness,
 		propMiss = missingness/nrow(x)
 		)
-  
-  # prints the name of the items with ":" next to it for order=FALSE
-	item.names = paste0(missData$item, ":")
+
 
   # when option=T, this inner function will be applied to data frame
 	if(order == TRUE){
 		missData = missData[order(missData$numMiss), ]
-    # overwrites previous item.names so that item matches numMiss and propMiss in output for order=TRUE
-    item.names = paste0(missData$item, ":")
+
 		}
   
-	# This will print the output (second and third column) using item.names (which varies from order=T/F) as row names
-	# digits and print.gap makes the output look pretty
-  return(print(missData[2:3], digits = 2, print.gap = 1L, row.names = item.names))
+  return(missData[,-1])
 
 	}
 
 
 #item.response.data <- read.csv("C:/Users/Kory/Desktop/R group/code a thon/math.csv", na.strings="")
+#item.response.data1 <- item.response.data[,-1]
 #miss(item.response.data, order=F)
 
 
