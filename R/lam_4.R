8102ab2687de9851e7deff4423f63947f151cf71
vuexx199 <vuexx199@umn.edu>
2014-10-20 12:30:32 -0500

    improve lambda 4
    added split half function into obtaining coefficient

--- R/lam_4.R
+++ R/lam_4.R
@@ -1,15 +1,35 @@
 
 
 
-
-lam_4 <-function(xa,xb)
+# have the ... so I need to call the split half function
+lam_4 <-function(x,oddEven=TRUE,...)
 {
   
-  var.xa <- var(apply(xa,1,sum))
-  var.xb <- var(apply(xb,1,sum))
-  c.ab <- cov(xa,xb)
+  if(oddEven == TRUE){         #I had a 1 but verify this
+    even.items = x[ , 1:length(x)%%2 == 0]
+    odd.items = x[ , 1:length(x)%%2 == 1]
+    even.score = rowSums(even.items)
+    odd.score = rowSums(odd.items)
+    varOdd = var(odd.score)
+    varEven = var(even.score)
+    covEO = cov(even.score, odd.score)
+    lam.4 <- 4*cov/(varOdd +varEven+2*cov%*%varOdd%*%varEven)
+    
+  } else{
+    samp.items = sample(1:length(x), length(x)/2, replace = FALSE)
+    form.a = x[ , samp.items]
+    form.b = x[ , -samp.items]
+    form.a.score = rowSums(form.a)
+    form.b.score = rowSums(form.b)
+    varA = var(form.a.score)
+    varB = var(form.b.score)
+    covAB = cov(form.a.score,form.b.score)
+    lav.4 = 4*cov/(varA + varB + 2*covAB%*%varA%*%varB)
+  }	
   
-  lam.4 <- 4*c.ab/(var.xa +var.xb+2*c.ab%*%var.xa%*%var.xb)
+  
+
+
   return(lam.4)
 }
 
@@ -20,3 +40,8 @@ library(devtools)
 
 
 
+m2 <- cbind(1, 1:4)
+colnames(m2, do.NULL = FALSE)
+colnames(m2) <- c("x","Y")
+rownames(m2) <- rownames(m2, do.NULL = FALSE, prefix = "Obs.")
+m2
