67cf1561b336e95ae01e52bcab905f6a6465e567
Andrew Zieffler <zief0002@umn.edu>
2014-11-07 15:06:27 -0600

    removed code from Feldt-Brennan

--- R/feldt_brennan.R
+++ R/feldt_brennan.R
@@ -4,46 +4,8 @@
 
 feldt_brennan = function(x, ...){
   
-  # create covariance matrix of the data frame
-  cov_matrix = cov(x, use = "pairwise.complete.obs")
-  
-  # Get the variances
-  variances = diag(cov_matrix)
+# No idea
 
-  # Compute the row sums of the off-diagonal elements
-  sum_of_row_covs = rowSums(cov_matrix) - variances
-  
-  # Compute the sums of the covariances and the variances
-  total_cov = sum(sum_of_row_covs)
-  total_var = sum(cov_matrix)
-
-  # Which row has the highest sum of covariances?
-  myRow = which.max(sum_of_row_covs)
-
-  # Get that row
-  d = cov_matrix[myRow, ]
-
-  # Compute D, Q, and W
-  D = (sum_of_row_covs - d) / (max(sum_of_row_covs) - d)
-  Q = sum(D) ^ 2
-  W = sum(D ^ 2)
-
-  fg = Q / (Q - W) * total_cov/total_var
-
-  # Compute CI based on Feldt's (1965) method
-  k = nrow(cov.matrix)
-  n = nrow(x)
-
-  df_1 = n - 1
-  df_2 = (n - 1) * (k - 1)
-
-  lower_limit = 1 - ((1 - fg) * qf(0.975, df_1, df_2))
-  upper_limit = 1 - ((1 - fg) * qf(0.0255, df_1, df_2))
-
-  # Compute standard error measurement
-  sem = sqrt(tot_var * (1 - fg))
-
-  return(list(alpha = fg, ll = lower_limit, ul = upper_limit, sem = sem))
 }
 
 # library(QME)
@@ -51,4 +13,4 @@ feldt_brennan = function(x, ...){
 # data(math_key)
 # out = QMEtest(math, math_key)
 # x = getKeyedTestNoID(out)
-# feldt_gilmer(x)
+# feldt_brennan(x)
