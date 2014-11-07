0160c914810fb1b4d03876a9e5fabde87d0ee0f2
knickodem <nicko013@umn.edu>
2014-11-07 13:57:58 -0600

    Convert blanks to NAs for missing data analysis

--- R/miss.R
+++ R/miss.R
@@ -5,7 +5,10 @@
 
 miss = function(x, order = FALSE,...){
   
-  # x is a data frame with and ID variable column of raw responses
+  #Convert blanks ("") to NA's
+  x[x == ""] = NA
+  
+  # x is a data frame with an ID variable column of raw responses
   # assume that missing can be detected; so no blank spaces as levels
   missingness = apply(x[ , 1:length(x)], MARGIN = 2, function(x) sum(is.na(x)))
   
@@ -31,5 +34,3 @@ miss = function(x, order = FALSE,...){
 #item.response.data <- read.csv("C:/Users/Kory/Desktop/R group/code a thon/math.csv", na.strings="")
 #item.response.data1 <- item.response.data[,-1]
 #miss(item.response.data, order=F)
-
-
