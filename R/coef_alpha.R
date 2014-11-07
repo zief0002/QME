0b9b16d88a77f7df67ed43fdcd7fb6e20bae6fe1
Andrew Zieffler <zief0002@umn.edu>
2014-11-07 16:45:23 -0600

    Updated coef_alpha

--- R/coef_alpha.R
+++ R/coef_alpha.R
@@ -2,14 +2,12 @@
 ## Function to produce coefficient alpha from test data
 ##############################################################
 
-
-
-
 coef_alpha = function(x, ...){
   
   # create covariance matrix of the data frame
   cov.matrix = cov(x, use = "pairwise.complete.obs")
   
+
   # collect the number of rows from the covariance matrix
   k = nrow(cov.matrix)
 
@@ -27,6 +25,7 @@ coef_alpha = function(x, ...){
   upper_limit = 1 - ((1 - alpha) * qf(0.0255, df_1, df_2))
 
   # Compute standard error measurement
+  tot_var = sum(cov.matrix)
   sem = sqrt(tot_var * (1 - alpha))
 
   return(list(alpha = alpha, ll = lower_limit, ul = upper_limit, sem = sem))
--- R/feldt_brennan.R
+++ R/feldt_brennan.R
@@ -4,8 +4,34 @@
 
 feldt_brennan = function(x, ...){
   
-# No idea
+	# create covariance matrix of the data frame
+	cov_matrix = cov(x, use = "pairwise.complete.obs")
+  
+	# Get the variance for the scores
+	score_var = sum(cov.matrix)
 
+	# Get the item variances
+	item_var = sum(diag(cov.matrix))
+
+	# Compute sum of the squared row sums
+	sq_row_sums = sum(rowSums(cov.matrix) ^ 2)
+
+	fb = (tot_var * (tot_var - obs_var)) / ((tot_var ^ 2 - sq_row_sums))
+
+	# Compute CI based on Feldt's (1965) method
+	k = nrow(cov.matrix)
+	n = nrow(x)
+
+	df_1 = n - 1
+	df_2 = (n - 1) * (k - 1)
+
+	lower_limit = 1 - ((1 - fb) * qf(0.975, df_1, df_2))
+	upper_limit = 1 - ((1 - fb) * qf(0.0255, df_1, df_2))
+
+	# Compute standard error measurement
+	sem = sqrt(tot_var * (1 - fb))
+
+	return(list(fb = fb, ll = lower_limit, ul = upper_limit, sem = sem))
 }
 
 # library(QME)
@@ -14,3 +40,4 @@ feldt_brennan = function(x, ...){
 # out = QMEtest(math, math_key)
 # x = getKeyedTestNoID(out)
 # feldt_brennan(x)
+
