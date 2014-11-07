40f062aa640a797993b2c600d741273495872cb6
Andrew Zieffler <zief0002@umn.edu>
2014-11-07 16:47:16 -0600

    Updated reliability function

--- R/reliability.R
+++ R/reliability.R
@@ -13,13 +13,14 @@ reliability = function(x, ...) {
     coef_alpha = coef_alpha,   # returns estimate, CI limits, and SEM
     guttman_l2 = guttman_l2,   # returns estimate, CI limits, and SEM
     guttman_l4 = guttman_l4,   # returns estimate, CI limits, and SEM
-    feldt_gilmer = feldt_gilmer   # returns estimate, CI limits, and SEM
+    feldt_gilmer = feldt_gilmer,   # returns estimate, CI limits, and SEM
+    feldt_brennan = feldt_brennan   # returns estimate, CI limits, and SEM
   )
   
   out = lapply(rel_funs, function(f) f(keyed_test_no_id))
   out = do.call(rbind, out)
   dimnames(out)[[2]] = c("Estimate", "95% LL", "95% UL", "SEM")
-  dimnames(out)[[1]] = c("Coefficient Alpha", "Guttman's L2", "Guttman's L4", "Feldt-Gilmer")
+  dimnames(out)[[1]] = c("Coefficient Alpha", "Guttman's L2", "Guttman's L4", "Feldt-Gilmer", "Feldt-Brennan")
 
   return(out)
   
