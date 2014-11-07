32c657eb0077c3bf18c61b43433007b7f703de43
knickodem <nicko013@umn.edu>
2014-11-07 15:46:15 -0600

    Item Difficulty
    Added calculation of item difficulty and printed output

--- future/item_level.R
+++ future/item_level.R
@@ -5,6 +5,11 @@
 #' @param x a \code{QMEtest} object
 #' @return A list with various aspects of the item level analysis
 
-item_level = function(x, ...) {
-  return(list()) # Currently does nothing
+item_level = function(keyed_test, ...) {
+  difficulty = colSums(keyed_test[ , 2:ncol(keyed_test)])/nrow(keyed_test)
+  il = matrix(c(difficulty))
+  rownames(il) = colnames(keyed_test[ , 2:ncol(keyed_test)])
+  colnames(il) = c("Difficulty")
+  print(il)
 }
+
