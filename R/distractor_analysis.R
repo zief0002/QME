74043134672cfdb2f4a9c0d71813f434f1d12b34
knickodem <nicko013@umn.edu>
2014-11-09 15:47:52 -0600

    Updated distractor_analysis
    Moved the distractor_difficulty code from item_analysis to the
    distractor_analysis function

--- R/distractor_analysis.R
+++ R/distractor_analysis.R
@@ -5,6 +5,11 @@
 #' @param x a \code{QMEtest} object
 #' @return A list with various aspects of the distractor analysis
 
-distractor_analysis = function(x, ...) {
-  return(list()) # Currently does nothing
+distractor_analysis = function(test, keyed_test, ...) {
+  distractors_difficulty = vector("list", length = ncol(test)-1)
+    for(i in 1:(ncol(test)-1)){
+      distractors_difficulty[[i]] = prop.table(table(test[i+1]))
+    }
+  names(distractors_difficulty)<-sprintf("Dist_diff of Item%i",1:(length(math)-1))
+  return(distractors_difficulty)
 }
