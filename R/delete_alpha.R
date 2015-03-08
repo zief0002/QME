86878e2a6df36c42fb99023b01f1bb110619a41c
Ethan Brown <ethancbrown@gmail.com>
2015-03-07 23:47:15 -0600

    fix deleted alpha function

--- R/delete_alpha.R
+++ R/delete_alpha.R
@@ -2,9 +2,11 @@
 ## delete_reliability is aimed to calculate the reliability with each item removed from the test
 delete_alpha<-function(x)
 {
-  delete_alpha<-c()
+  deleted_alpha<-c()
   for (j in 1:ncol(x))
   {
-    delete_alpha[j]<-coef_alpha(x[,-j])$alpha
+    deleted_alpha[j]<-coef_alpha(x[,-j])$alpha
   }
+  names(deleted_alpha) = names(x)
+  deleted_alpha
 }
