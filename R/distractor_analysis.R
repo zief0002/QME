14fd8139b2bfefc34dfc9459cda20d749c97a65b
Andrew Zieffler <zief0002@umn.edu>
2015-03-13 13:50:53 -0500

    Updated distractor_analysis function

--- R/distractor_analysis.R
+++ R/distractor_analysis.R
@@ -10,10 +10,30 @@ distractor_analysis = function(testQME, ...) {
   num_items = ncol(raw_test)
   
   distractors_difficulty = vector("list", length = num_items)
+  distractors_discrim = vector("list", length = num_items)
+
   for(i in 1:num_items){
     distractors_difficulty[[i]] = prop.table(table(raw_test[i]))
+
+    ## data frame of responses to item i, ttem-deleted total score, tercile
+		new = data.frame(resp[ , i], delscores[ , i], terciles[ , i])
+		names(new) = c("response", "corrected_score", "tercile")
+
+		## Loop through to create variables holding the 0/1 for each response
+		j = length(levels(new$response))
+		myCorrs = rep(NA, j)
+	
+		for(k in 1:j){
+			new[ , (3+k)] = ifelse(new$response == levels(new$response)[k], 1, 0)
+			names(new)[3+k] = levels(new$response)[k] 
+			myCorrs[k] = cor(new$corrected_score, new[ ,(3+k)])
+		}
+	distractors_discrim[[i]] = myCorrs 	
+
+
   }
-  names(distractors_difficulty)<-sprintf("Dist_diff of Item%i",1:num_items)
+  names(distractors_difficulty) = sprintf("Dist_diff of Item%i",1:num_items)
+  names(distractors_discrim) = sprintf("Dist_disc of Item%i",1:num_items)
   
-  return(distractors_difficulty)
+  return(list(distractors_difficulty, distractors_discrim))
 }
