cf8b338022a7098dd6c73d61440dc06e5d252ab0
Kyungin <park1340@umn.edu>
2015-03-13 15:46:06 -0500

    Help for psycho_report

--- R/psycho_report.R
+++ R/psycho_report.R
@@ -1,3 +1,12 @@
+#' Create a test report
+#' 
+#' This function creates a report with the basic psychometrics
+#'
+#' @param x An \code{odin_zeus} object
+#' @param output The file name of the html report
+#' @param open Whether to open the html report
+#' @return As a side effect, creates the html report 
+
 psycho_report = function(x, output = "output.html", open = TRUE) {
   ## Inputs an odin_zeus, creates report, returns filename
   thistest = x
