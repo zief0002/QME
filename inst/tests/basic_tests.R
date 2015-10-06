06bcb37a36eb985e09a8f371d6a86eeecc03583c
Erik Anderson <erikbpanderson@gmail.com>
2015-10-06 17:21:08 -0500

    Moved general test to be non-distributable and runnable via devtools defaults

--- inst/tests/basic_tests.R
+++
@@ -1,9 +0,0 @@
-
-require(testthat)
-
-data(math)
-data(math_key)
-
-test_that("odin_zeus runs without errors", {
-  oz = odin_zeus(math, math_key)
-})
---
+++ tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(QME)
+
+test_check("QME")
