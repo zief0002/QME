edcae33e89c08d104e00c6b365aa5ac75fb159bc
knickodem <nicko013@umn.edu>
2015-10-22 17:50:12 -0500

    New tests for is_valid_key and refine_key

--- tests/testthat/test-footprint.R
+++ tests/testthat/test-footprint.R
@@ -6,4 +6,22 @@ data(math_key)
 
 test_that("odin_zeus runs without errors", {
   oz = odin_zeus(math, math_key)
-})
+print("yo")
+  })
+
+test_that("Is the key a valid dataframe", {
+  goodkey = data.frame(response = c("A","B","C"),
+                           Q1 = c(1,0,0),
+                           Q2 = c(0,NA,1),
+                           Q3 = c(0, .5, 1))
+  badkey = data.frame(response = c("A","B","C"),
+                      Q1 = c(1,0,"bum"),
+                      Q2 = c(0,NA,1),
+                      Q3 = c(0, .5, 1))
+    expect_true(is_valid_key(goodkey))
+    expect_false(is_valid_key(badkey))
+    expect_false(TRUE)
+    expect_true(FALSE)
+print("foo")})
+
+test_that("Convert key vector into key dataframe")
