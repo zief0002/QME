82d0a327f0f2d61a1310234e4dea87d44502c340
knickodem <nicko013@umn.edu>
2016-01-01 16:17:02 -0600

    Correcting right_wrong()
    changing new_key to key

--- R/right_wrong.R
+++ R/right_wrong.R
@@ -3,9 +3,9 @@
 #########################################
 right_wrong = function(test_with_id, key) {
   
-  score_column = function(itemcol, colname, new_key) {
+  score_column = function(itemcol, colname, key) {
     ## Scores a single column
-    scores = new_key[match(itemcol, new_key$response), colname]
+    scores = key[match(itemcol, key$response), colname]
     scores[is.na(scores)] = 0 #maybe put a warning here?
     
     scores
@@ -14,7 +14,7 @@ right_wrong = function(test_with_id, key) {
   test_no_id = test_with_id[,-1]
   
   scored_matrix = vapply(names(test_no_id), function(colname) {
-    score_column(test_no_id[[colname]], colname, new_key)
+    score_column(test_no_id[[colname]], colname, key)
   }, FUN.VALUE = rep(0, nrow(test_no_id)))
   
   data.frame(id = test_with_id$id, scored_matrix)
