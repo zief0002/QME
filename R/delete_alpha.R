c6b0468c5cc3a32ba16a49384e50454ba74d64c9
Ethan C. Brown <ethancbrown@gmail.com>
2016-06-17 17:47:29 -0500

    use vapply for delete_alpha (resolves #38)

--- R/delete_alpha.R
+++ R/delete_alpha.R
@@ -1,12 +1,10 @@
 ################################################################################
-## delete_reliability is aimed to calculate the reliability with each item removed from the test
-delete_alpha<-function(x)
-{
-  deleted_alpha<-c()
-  for (j in 1:ncol(x))
-  {
-    deleted_alpha[j]<-coef_alpha(x[,-j])[["alpha"]]
-  }
+## delete_reliability is aimed to calculate the reliability with each item removed from the (scored) text x
+delete_alpha<-function(x) {
+  deleted_alpha = vapply(1:ncol(x), 
+                         function(j)
+                           coef_alpha(x[,-j])[["alpha"]],
+                         NA_real_)
   names(deleted_alpha) = names(x)
   deleted_alpha
 }
