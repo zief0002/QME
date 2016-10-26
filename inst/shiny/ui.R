433e3804731b641565c97fbe0f7b5629c6a82a49
vuexx199 <vuexx199@umn.edu>
2016-10-26 11:02:11 -0500

    update from rstudio
    try to make this work

--- inst/shiny/ui.R
+++ inst/shiny/ui.R
@@ -8,6 +8,7 @@
 library(shiny)
 library(QME)
 
+
 shinyUI(fluidPage(
   sidebarPanel(
     fileInput('file1', 'Bring in CSV File',
@@ -15,12 +16,25 @@ shinyUI(fluidPage(
     fileInput('file2', 'Bring in CSV key File',
               accept=c('text/csv', 'text/comma-separated-values,text/plain')),
     tags$hr(),
-    checkboxInput('rownames', 'Data ID Column', TRUE)
+    # checkboxInput('header', 'Data Header', TRUE),
+    checkboxInput('rownames', 'Data ID Column', TRUE),
+    # radioButtons('sep', 'Data Separator:', c(Comma=',',Semicolon=';',Tab='\t'),
+    #              ','),
+    # radioButtons('quote', 'Data Quote:',
+    #              c(None='','Double Quote'='"','Single Quote'="'"),
+    #              '"'),
+    tags$hr() #,
+    # checkboxInput('header1', 'Key Header', TRUE),
+    # radioButtons('sep1', 'Key Separator:', c(Comma=',',Semicolon=';',Tab='\t'),
+    #              ','),
+    # radioButtons('quote1', 'Key Quote:',
+    #              c(None='','Double Quote'='"','Single Quote'="'"),
+    #              '"')
   ),
   mainPanel(
     tabsetPanel(
       tabPanel("Data",
-              br(),
+               br(),
                br(),br(),
                tableOutput('datafile')
       ),
