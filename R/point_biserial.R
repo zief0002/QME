b3e0cbf5d987de943767f254738a6c56f06b81fa
knickodem <nicko013@umn.edu>
2016-12-02 16:50:58 -0600

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	R/point_biserial.R

--- R/point_biserial.R
+++ R/point_biserial.R
@@ -17,13 +17,9 @@ point_biserial = function(x, ...) {
   
   
   pbis_report = data.frame(Item = colnames(x),
-                           point_biserial = NA_real_,
-                           corrected_pbis = NA_real_)
+                           point_biserial = NA_real_)
   
   pbis_report$point_biserial[positive_sd] = cor(total_score, x[, positive_sd])[1,]
-  pbis_report$corrected_pbis[positive_sd] = mapply(cor,
-                                                   delscores[, positive_sd], 
-                                                   x[, positive_sd])
   
 
   return(pbis_report)
