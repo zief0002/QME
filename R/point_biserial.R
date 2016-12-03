4c5e5e6d3cfdd00836e7f49f53f0a5c13fd28423
knickodem <nicko013@umn.edu>
2016-12-02 20:28:43 -0600

    Remove PBiS from point_biserial.R

--- R/point_biserial.R
+++ R/point_biserial.R
@@ -6,7 +6,7 @@ point_biserial = function(x, ...) {
   total_score = rowSums(x)
   delscores = total_score - x
   
-  ## Only calculate pbis and corrected pbis when sd > 0
+  ## Only calculate corrected pbis when sd > 0
   positive_sd = vapply(x,
                        function(y) length(unique(y)) > 1,
                        TRUE)
@@ -17,9 +17,11 @@ point_biserial = function(x, ...) {
   
   
   pbis_report = data.frame(Item = colnames(x),
-                           point_biserial = NA_real_)
+                           corrected_pbis = NA_real_)
   
-  pbis_report$point_biserial[positive_sd] = cor(total_score, x[, positive_sd])[1,]
+  pbis_report$corrected_pbis[positive_sd] = mapply(cor,
+                                                   delscores[, positive_sd],
+                                                   x[, positive_sd])
   
 
   return(pbis_report)
