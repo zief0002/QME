8c3406a9e88984110fc9eb1e0f81bac0e5eb81c0
knickodem <nicko013@umn.edu>
2016-12-02 20:30:37 -0600

    Removing deleted alpha and PBiS from analyze

--- R/analyze_methods.R
+++ R/analyze_methods.R
@@ -12,17 +12,17 @@ getReliability = function(x)
   x$test_level$reliability
 
 ## Get the deleted alphas from an \code{analyze} object
-getDelAlpha = function(x) {
-  dalph = data.frame(x$item_level$del_alphas)
-  names(dalph) = "Alpha (if Item Deleted)" 
-  dalph
-}
+# getDelAlpha = function(x) {
+#   dalph = data.frame(x$item_level$del_alphas)
+#   names(dalph) = "Alpha (if Item Deleted)" 
+#   dalph
+# }
 
 ## Get the item overview for a \code{analyze} object
 getItemOverview = function(x) {
   
   overview = data.frame(x$item_level$item_stats, x$item_level$missing)
-  names(overview) = c("Difficulty", "PBiS", "Corrected PBiS", "Missing (N)", "Missing (p)")
+  names(overview) = c("Difficulty", "Corrected PBiS", "Missing (N)", "Missing (p)")
   overview
 }
 
--- R/analyze_summary.R
+++ R/analyze_summary.R
@@ -3,7 +3,7 @@
 summary.analyze = function(x, ...) {
   print(x)
   cat("\n")
-  print(getDelAlpha(x), digits = 2, print.gap = 1L)
+  # print(getDelAlpha(x), digits = 2, print.gap = 1L)
   cat("\nItem overview\n")
   cat("-------------------------------\n")
   print(getItemOverview(x), digits = 2, print.gap = 1L)
