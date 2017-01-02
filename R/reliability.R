89b38ddc5540094689db6906eb66d17b49c45b6f
knickodem <nicko013@umn.edu>
2017-01-02 11:10:48 -0600

    Cautionary Note about Coefficient Alpha
    A cuationary note was added to the report template and Coefficient Alpha
    was moved to the bottom of the reliability table to reduce usage.

--- R/reliability.R
+++ R/reliability.R
@@ -11,17 +11,17 @@ reliability = function(x, ...) {
   keyed_test_no_id = getKeyedTestNoID(x)
   
   rel_funs = list(
-    coef_alpha = coef_alpha,   # returns estimate, CI limits, and SEM
     guttman_l2 = guttman_l2,   # returns estimate, CI limits, and SEM
     guttman_l4 = guttman_l4,   # returns estimate, CI limits, and SEM
     feldt_gilmer = feldt_gilmer,   # returns estimate, CI limits, and SEM
-    feldt_brennan = feldt_brennan   # returns estimate, CI limits, and SEM
+    feldt_brennan = feldt_brennan,   # returns estimate, CI limits, and SEM
+    coef_alpha = coef_alpha   # returns estimate, CI limits, and SEM
   )
   
   out = lapply(rel_funs, function(f) f(keyed_test_no_id))
   out = do.call(rbind, out)
   dimnames(out)[[2]] = c("Estimate", "95% LL", "95% UL", "SEM")
-  dimnames(out)[[1]] = c("Coefficient Alpha", "Guttman's L2", "Guttman's L4", "Feldt-Gilmer", "Feldt-Brennan")
+  dimnames(out)[[1]] = c("Guttman's L2", "Guttman's L4", "Feldt-Gilmer", "Feldt-Brennan","Coefficient Alpha")
 
   return(as.data.frame(out))
   
