74b04b9fb9ece06e5a219a0c0ff1493f22a581f2
Ethan Brown <brow3821@umn.edu>
2017-04-13 12:42:25 -0500

    add tests for #58 and #59

--- tests/testthat/tests-failing.R
+++ tests/testthat/tests-failing.R
@@ -1,9 +1,6 @@
 require(testthat)
 require(QME)
 
-data(math)
-data(math_key)
-
 math_qt = QMEtest(math, math_key)
 oz = analyze(math, math_key)
 
@@ -29,5 +26,29 @@ test_that("refine key includes all possible response options", {
   expect_equal(goodkey[order(goodkey$response),],
                response_qt$key[order(response_qt$key$response), ])
       })
-  
 
+
+# More input testing ------------------------------------------------------
+math_char = math
+math_char[,-1] = apply(math_char[,-1], 2, as.character)
+
+names(math_char)[3] = "different_from_key"
+
+test_that("QMEtest checks whether test and key have same column names", {
+  expect_error(QMEtest(math_char, math_key))
+})
+
+math_char = math
+math_char[,-1] = apply(math_char[,-1], 2, as.character)
+
+math_char[2, 2] = "other option"
+
+test_that("refine_key catches all response options", {
+  refined_key = QME:::refine_key(math_key, math_char)
+  expect_true("other option" %in% refined_key$response)
+})
+
+test_that("QMEtest ensures that key has the same response options as test", {
+  expect_error(QMEtest(math_char, math_key))
+  }
+)
